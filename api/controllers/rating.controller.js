71e4b7569988054395be5909de0432c84cda7dac
likhith1072 <150714530+likhith1072@users.noreply.github.com>
2025-06-21 09:15:04 +0530

    stoping duplicate ratings from user

--- api/controllers/rating.controller.js
+++ api/controllers/rating.controller.js
@@ -1,25 +1,38 @@
 import Rating from "../models/rating.model.js";
+import { errorHandler } from "../utils/error.js";
 
-export const createRating = async (req, res) => {
+export const createRating = async (req, res, next) => {
     try {
-        const { bookId, userId ,rating } = req.body;
-        
-        if(userId !== req.user.id){
-          return next(errorHandler(403,"You are not allowed to rate this book"));
+        const { bookId, userId, rating } = req.body;
+
+        if (userId !== req.user.id) {
+            return next(errorHandler(403, "You are not allowed to rate this book"));
         }
-            
-        const newRating = new Rating({
-        bookId,
-        userId,
-        rating,
-        });
-        await newRating.save();
-       
-        res.status(200).json(newRating);
+
+        // Check if rating already exists
+        const existingRating = await Rating.findOne({ bookId, userId });
+
+        let savedRating;
+
+        if (existingRating) {
+            // Update existing rating
+            existingRating.rating = rating;
+            savedRating = await existingRating.save();
+        } else {
+            // Create new rating
+            const newRating = new Rating({
+                bookId,
+                userId,
+                rating,
+            });
+            savedRating = await newRating.save();
+        }
+
+        res.status(200).json(savedRating);
     } catch (error) {
         next(error);
     }
-}
+};
 
 
 export const getBookRatings=async( req,res,next)=>{
@@ -30,4 +43,5 @@ export const getBookRatings=async( req,res,next)=>{
         next(error);
     }
 }
+
 
